e58f38ecc9bfbe128a3c7063eff7e2eeeb7f002b
B0lu <boluelbot@gmail.com>
2020-05-08 01:06:50 +0200

    Too tired to fix the execs with execSyncs.

--- commands/test_cuda.js
+++ commands/test_cuda.js
@@ -2,13 +2,41 @@ const fs = require("fs");
 const { exec } = require("child_process");
 
 exports.run = async (bot, msg) => {
-	const compilation = exec(`cd ../programs; make`, (error, stdout, stderr) => {
+	exec(`cd ./programs; make`, async (error, stdout, stderr) => {
+		fs.unlinkSync(`./programs/${process.env.PROGRAM}`);
 		if (error) throw error;
-		if (stderr) return msg.reply(`COMPILATION ERROR:\n\n${stderr}`);
+		if (stderr) return await msg.reply(`COMPILATION ERROR:\n\n${stderr}`);
+
+		let tests = fs.readdirSync("./tests/");
+		for (i = 0; tests.includes(`${i}.sh`); i++) {
+			exec(`./tests/${i}.sh`, async (error, stdout, stderr) => {
+				if (error) throw error;
+				if (stderr) {
+					let current = fs.readFileSync(`./tests/${i}.sh`);
+					msg.reply(`test ${i + 1}: EXECUTION ERROR: \n\n${stderr}\n\nTest was: ${current.substring("../programs/".length)}`);
+				}
+				else if (stdout) {
+					fs.writeFileSync(`./outputs/${i}.txt`, stdout);
+
+					exec(`diff ./outputs/o${i}.txt ./outputs/${i}.txt`, async (error, stdout, stderr) => {
+						if (error) throw error;
+						if (stderr) msg.reply(`your program is so lame, it made a diff give an error.`);
+						if (stdout) {
+							let lines = stdout.split("\n");
+
+							if (!stdout.startsWith("2c2")) {
+								let current = fs.readFileSync(`./tests/${i}.sh`);
+								msg.reply(`test ${i + 1}: Failed.\n\nExpected: ${lines[2].substring(2)}\nGot: ${lines[5].substring(2)}\n\nTest was: ${current.substring("./programs/".length)}`);
+							} else {
+								msg.reply(`test ${i + 1}: Passed.\n\n${lines[3]}`);
+							}
+						}
+						fs.unlinkSync(`./outputs/${i}.txt`);
+					});
+				}
+			});
+		}
+
+		exec(`cd ../programs; make clean`, (error, stdout, stderr) => {});
 	});
-	fs.unlink(`../programs/${PROGRAM}`, err => { if (err) throw err; });
-
-	// WIP
-
-	const clean = exec(`cd ../programs; make clean`, (error, stdout, stderr) => {});
 }
--- index.js
+++ index.js
@@ -7,18 +7,24 @@ const bot = new Discord.Client();
 bot.login(process.env.TOKEN);
 
 
-bot.on("message", msg => {
+bot.on("message", async msg => {
 	if (msg.author.bot) return;
 
 	if (msg.attachments.size > 0 && msg.channel.name.startsWith(process.env.REQ_CHANNEL)) {
 		// TODO: add the option to send privately.
-		let url = msg.attachments.first().url;
-		msg.delete();
-		let queue = msg.channel.name.substring(process.env.REQ_CHANNEL);
+		let att = msg.attachments.first();
+
+		if (!att.name.match(".cu?$"))
+			return msg.reply("only .c and .cu files are allowed.");
+
+		let queue = msg.channel.name.substring(process.env.REQ_CHANNEL.length);
 
 		const file = fs.createWriteStream(`./programs/${process.env.PROGRAM}`);
-		const request = http.get(url, response => response.pipe(file));
-		require(`./commands/test${queue}.js`).run(bot, msg);
+		const request = await http.get(att.url, async response => {
+			response.pipe(file)
+			await msg.delete();
+			require(`./commands/test${queue}.js`).run(bot, msg);
+		});
 	}
 
 	else if (msg.content.startsWith(process.env.PRE)) {
